5d870806f43080ca9129ba65d1dcd84b5c4c8da3
Cory Eighan <zephyr834@gmail.com>
2019-02-04 14:48:55 -0800

    Skeleton and styling of Navbar.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar'
 import ProductList from './components/ProductList'
 import Default from './components/Default'
 import Cart from './components/Cart'
-import Details from './components/Details'
+import Details from './components/product/ProductDetails'
 
 
 class App extends Component {
@@ -17,7 +17,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={ProductList}/>
           <Route path="/details" component={Details}/>
-          <Route path="/store" component={Cart}/>
+          <Route path="/cart" component={Cart}/>
           <Route component={Default}/>
         </Switch>
       </React.Fragment>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Product from './Product'
+import Product from './product/Product'
 export default class ProductList extends Component {
   render() {
     return (
